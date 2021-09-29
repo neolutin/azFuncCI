72a3c7b1d0a1b6e96726133a26bf8e7f32c88f84
Lebret Soler Vincent (ENGIE Global Markets SAS) <SN5457@engie.com>
2021-09-29 20:05:30 +0200

    Add AzFunc project

---
+++ FunctionCompute/Compute.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace FunctionCompute
+{
+    public static class Compute
+    {
+        [FunctionName("ComputeData")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name ??= data?.name;
+
+            var dataApi = GetData(log);
+            var responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}: here is the data: {dataApi}.";
+
+            return new OkObjectResult(responseMessage);
+        }
+        
+        private static Task GetData(ILogger log)
+        {
+            var httpClient = new HttpClient();
+            var dataApiUrl = Environment.GetEnvironmentVariable("DataApiUrl", EnvironmentVariableTarget.Process);
+            
+           return httpClient.GetAsync(dataApiUrl).ContinueWith(r =>
+               {
+                   r.Result.Content.ReadAsStringAsync().ContinueWith(s =>
+                   {
+                       log.LogInformation(s.Result);
+                       return s.Result;
+                   });
+               }
+            );
+        }
+    }
+}
+
